71b05ca7e5cb99907815f5ad40b0d799e6663d73
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-04-08 02:27:01 -0400

    add hover effect to profile picture

--- js/main.js
+++ js/main.js
@@ -5,12 +5,14 @@
     }, function () {
         $('#profile_pic').css('filter', 'grayscale(100%)');
     });
-});*/
+});
 
 $(document).ready(function () {
-    $('#profile_pic').hover(function () {
+
+    $('#profile_pic').on('click', function () {
         $('#profile_pic').addClass('color');
-    }, function () {
+    }).on('click', function () {
         $('#profile_pic').removeClass('color');
     });
-});
+
+});*/
