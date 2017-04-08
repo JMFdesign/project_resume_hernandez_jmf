a7d7b8c0ec79239a9ff1166609536a03027ffd56
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-04-08 02:58:41 -0400

    add custom jQuery - alert window 'thank you' for visiting  my site

--- js/main.js
+++ js/main.js
@@ -1,18 +1,6 @@
-/*$(document).ready(function () {
-
-    $('#profile_pic').hover(function () {
-        $('#profile_pic').css('filter', 'grayscale(0%)');
-    }, function () {
-        $('#profile_pic').css('filter', 'grayscale(100%)');
-    });
-});
-
 $(document).ready(function () {
 
-    $('#profile_pic').on('click', function () {
-        $('#profile_pic').addClass('color');
-    }).on('click', function () {
-        $('#profile_pic').removeClass('color');
+    $('#header').hover(function () {
+        alert('Thank you for visiting my Online Resume!');
     });
-
-});*/
+});
